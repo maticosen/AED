4ae060966c38639d39781af83f9d02cb8906c1f2
maticosen <63520387+maticosen@users.noreply.github.com>
2020-04-22 18:04:34 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,13 @@
+/*  Hello world
+    Matias Cosentino
+    19042020
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+/* Para capturar la salida en un archivo se ejecuto: ".\hello.exe > output.txt" */

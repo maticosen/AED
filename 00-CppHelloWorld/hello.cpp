4955d9383f8153fb4f90f203ec12d322e05c36f7
maticosen <63520387+maticosen@users.noreply.github.com>
2020-04-23 21:09:44 -0300

    Update hello.cpp

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,13 +1,12 @@
 /*  Hello world
     Matias Cosentino
-    19042020
+    23042020
 */
 
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!\n";
-    return 0;
 }
 
 /* Para capturar la salida en un archivo se ejecuto: ".\hello.exe > output.txt" */

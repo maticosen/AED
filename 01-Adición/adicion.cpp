198b95ab4defc03c6672912e3b710b9aa522e6c2
maticosen <63520387+maticosen@users.noreply.github.com>
2020-04-29 20:39:22 -0300

    Add files via upload

---
+++ 01-Adición/adicion.cpp
@@ -0,0 +1,17 @@
+/*  Adición
+    Matias Cosentino
+    26042020
+*/
+
+#include <iostream>
+
+int main() {
+
+    int a, b;
+
+    std::cout<<"Introduzca el primer entero: ";
+    std::cin>>a;
+	std::cout<<"Introduzca el segundo entero: ";
+    std::cin>>b;
+    std::cout<<"El resultado de la suma es: "<<a + b;
+}

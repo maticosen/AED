4b88800bb2faced5f04741b523b613cf95874668
maticosen <maticosen@users.noreply.github.com>
2020-11-25 19:52:54 -0300

    commit

--- 05-Geometria/Punto/PuntoTest.cpp
+++ 05-Geometria/Punto/PuntoTest.cpp
@@ -11,8 +11,8 @@ int main ()
     assert (not IsIgual({1,2},{3,4})); 
 
     assert (GetDistancia({1,-8},{7,0}) == 10);
-    assert (not GetDistancia({-2,-3},{-1,-4}) == 1.41);  //1,41421
+    assert (GetDistancia({-2,-3},{-1,-4}) != 1.41);  //1,41421
 
     assert (GetDistanciaAlOrigen({-5,12}) == 13);
-    assert (not GetDistanciaAlOrigen({1,1}) == 1.41);
+    assert (GetDistanciaAlOrigen({1,1}) != 1.41);
 }
